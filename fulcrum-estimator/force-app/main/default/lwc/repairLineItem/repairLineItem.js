a0531361a7b00e8d2030ada08b858f9696477428
Ryan Spicer <rspicer898@gmail.com>
2024-05-14 16:26:18 -0400

    Repair Line Item LWC created, basic functionality added, and hooked up to parent class.

---
+++ fulcrum-estimator/force-app/main/default/lwc/repairLineItem/repairLineItem.js
@@ -0,0 +1,44 @@
+import { LightningElement, api } from 'lwc';
+
+export default class RepairLineItem extends LightningElement {
+    @api name = '';
+    @api type = 'Materials';
+    @api price = 0;
+    @api notes = '';
+    @api id = '';
+
+    // Hard-coded picklist for Type of Line Item
+    // This could be pulled from our existing picklist. Hard-coded to save time.
+    // Value of each option matches our picklist API values for easy saving.
+    get typeOptions() {
+        return [
+            { label: 'Materials', value: 'Materials' },
+            { label: 'Labor', value: 'Labor' },
+            { label: 'All-Inclusive', value: 'All-Inclusive' },
+        ];
+    }
+
+    // Since our change handlers are similar in structure, we can condense them in to a generic handler using the name of the field
+    // We bubble the changes up to the main component to ensure the data is consistent and housed at the top level
+    handleGenericChange(e) {
+        this.dispatchEvent(new CustomEvent('changeitemvalues', {
+            detail: {
+                fieldName: e.target.name,
+                value: e.detail.value,
+                id: this.id
+            },
+            bubbles: true,
+            composed: true
+        }));
+    }
+
+    handleDelete(e) {
+        this.dispatchEvent(new CustomEvent('deleteitem', {
+            detail: {
+                id: this.id
+            },
+            bubbles: true,
+            composed: true
+        }))
+    }
+}
